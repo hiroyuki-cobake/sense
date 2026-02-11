7879ac83e645f356766c4480ee775c07d79fdec7
hiroyuki-cobake <hiroyuki.sugiura@cobake.co>
2026-02-11 07:30:27 +0000

    build: configure GitHub Pages output to docs

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "public",
+  base: "/sense/", // ← GitHub Pages用（超重要）
+
   server: {
     host: true,
     port: 3000,
     strictPort: true,
   },
+
   preview: {
     host: true,
     port: 3000,
     strictPort: true,
   },
+
   build: {
-    outDir: "../dist",
+    outDir: "../docs",   // ← publicの外にdocsを作る
     emptyOutDir: true,
   },
 });
