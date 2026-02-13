1edcb0ca863ae34b2fb7004ed5b0c6c34e479938
hiroyuki-cobake <hiroyuki.sugiura@cobake.co>
2026-02-13 15:32:14 +0000

    add random start preset for sense v1

--- public/js/audio/layers.js
+++ public/js/audio/layers.js
@@ -24,6 +24,48 @@ export const layers = (() => {
     tone.connect(toneGain);
     toneGain.connect(audio.master);
     tone.start();
+
+    startPreset();
+  }
+
+  function startPreset() {
+    const r = Math.floor(Math.random() * 8);
+
+    switch (r) {
+      case 0: // low ambience
+        setAmbience(0.02);
+        break;
+
+      case 1: // heavier ambience
+        setAmbience(0.06);
+        break;
+
+      case 2: // low tone faint
+        setTone(0.015, 130);
+        break;
+
+      case 3: // higher faint tone
+        setTone(0.02, 165);
+        break;
+
+      case 4: // mixed light ambience
+        setAmbience(0.03);
+        setTone(0.01, 150);
+        break;
+
+      case 5:
+        setAmbience(0.08);
+        break;
+
+      case 6:
+        setTone(0.025, 120);
+        break;
+
+      case 7:
+        setAmbience(0.01);
+        setTone(0.03, 180);
+        break;
+    }
   }
 
   function stop() {
